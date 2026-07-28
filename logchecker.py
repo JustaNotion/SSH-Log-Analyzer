0568b1947c1ef6eed5823e86a1e81b9e269d9764
Ian Lemus <117689708+JustaNotion@users.noreply.github.com>
2026-07-28 10:57:45 -0500

    Update

--- logchecker.py
+++ logchecker.py
@@ -7,7 +7,7 @@ with open("sample_auth.log") as f:
             ip = line.split("from ")[1].split(" port")[0]
             counts[ip] = counts.get(ip, 0) + 1
 
-ranked = sorted(counts.items(), key=lambda pair: pair[1], reverse = True)
+ranked = sorted(counts.items(), key=lambda pair: pair[1], reverse=True)
 for ip, count in ranked:
     if count >= 5:
         print(f"[WARNING]: The following IP: {ip} has failed {count} attempts")
